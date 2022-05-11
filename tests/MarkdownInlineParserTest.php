79698dc170f3fbcf3bc24877da9c6bbae0901fab
Morten D. Hansen <morten@visia.dk>
2022-05-11 14:32:08 +0200

    Adding test  - it in fact loads and is used

--- tests/MarkdownInlineParserTest.php
+++ tests/MarkdownInlineParserTest.php
@@ -2,7 +2,35 @@
 
 namespace Spatie\LaravelMarkdown\Tests;
 
+
+use Spatie\LaravelMarkdown\MarkdownRenderer;
+use Spatie\LaravelMarkdown\Tests\Stubs\CustomInlineParser;
+
 class MarkdownInlineParserTest extends TestCase
 {
+
+    /** @test */
+    public function it_can_use_custom_inline_parser()
+    {
+        config()->set('markdown.inline_parsers', [
+            ['parser' => new CustomInlineParser(), 'priority' => 0]
+        ]);
+
+        $markdown = <<<MD
+            I am a text with a very randomly placed @ symbol in the middle.
+           MD;
+
+        $html = $this
+            ->markdownRenderer()
+            ->disableAnchors()
+            ->toHtml($markdown);
+
+        $this->assertEquals('<p>I am a text with a very randomly placed THIS-REPLACED-THE-SYMBOL symbol in the middle.</p>' . PHP_EOL, $html);
+    }
+
+    protected function markdownRenderer(): MarkdownRenderer
+    {
+        return app(MarkdownRenderer::class);
+    }
 
 }
